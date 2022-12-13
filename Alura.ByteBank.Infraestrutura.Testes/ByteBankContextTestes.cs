b33de63296b465c117ddddfcd0aa4a1908ab3788
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-12-13 15:55:58 -0300

    test - criação do teste de conexão com o bd

---
+++ Alura.ByteBank.Infraestrutura.Testes/ByteBankContextTestes.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComBDMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Não foi possivel conectar ao banco de dados!");
+            }
+
+            //Assert
+            Assert.True(conectado);
+        }
+    }
+}
